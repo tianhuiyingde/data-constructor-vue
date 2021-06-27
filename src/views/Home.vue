a66dd655f52d654346d63f894f24e72d2c6b0b38
tianhuiying <tianhuiying@aixuexi.com>
2021-06-27 23:39:10 +0800

    修改bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,21 +7,24 @@
   </div>
 
   <div class="components-create" v-if="stepStatus == 0">
-    <a-input class="components-input" maxLength = 10 size="large" placeholder="请输入互动题名称" allow-clear @change="onChange" />
+    <a-input  class="components-input" maxLength = 10 size="large" placeholder="请输入互动题名称" allow-clear @change="onChange" />
     <a-button type="primary" :size="size" @click="nextStep1">
       创建
     </a-button>
   </div>
-
+  <div v-if="stepStatus == 4">
+    <a-spin size="large" />
+  </div>
   <div class="components-update" v-if="stepStatus == 1" :data-sourse="data">
     <div style="color: red;font-size: 20px;margin-top: 10px;">创建成功！请进入以下地址编辑互动题内容并保存。</div>
     <a class="target" target="_blank" :href=editorUrl >
-      互动题名称：{{gameName}}</a> 
+      互动题名称：{{inputVal}}</a> 
 
     </div>
 
   <div class="components-publish" v-if="stepStatus == 2">
-    <a-select :size="large"  style="width: 150px;margin-right: 20px;" placeholder="请选择学段" @change="handleChange" >
+    <span>学段：</span>
+    <a-select default-value="1" :size="large"  style="width: 150px;margin-right: 20px;" placeholder="请选择学段" @change="handleChangeSubject" >
       <a-select-option value="1">
         小学
       </a-select-option>
@@ -32,8 +35,8 @@
         高中
       </a-select-option>
     </a-select>
-
-    <a-select :size="large"  style="width: 150px;margin-right: 50px;" placeholder="请选择学科" @change="handleChange" >
+    <span>学科：</span>
+    <a-select default-value="1"  :size="large"  style="width: 150px;margin-right: 50px;" placeholder="请选择学科" @change="handleChangeSchoolSection" >
       <a-select-option value="1">
         语文
       </a-select-option>
@@ -53,7 +56,7 @@
   <div class="publish-success" v-if="stepStatus == 3" :data-sourse="data">
     <div style="color: red;font-size: 20px;margin-top: 10px;">互动题发布成功!去查看</div>
     <a class="target" target="_blank" :href=gameUrl >
-      互动题名称：{{gameName}}</a> 
+      互动题名称：{{inputVal}}</a> 
 
     </div>
 
@@ -92,43 +95,63 @@ export default defineComponent({
       ],
       editorUrl:'',
       gameUrl:'',
-      gameName:"1"
+      inputVal:'默认新互动题',
+      subjectVal:1,
+      schoolsectionVal:1
     });
 
     // onMounted(() => {
 
     // });
 
+    const onChange = (e: any) =>{
+      data.inputVal=e.target.value;
+    }
+
+    const handleChangeSubject = (value: any) =>{
+      data.subjectVal=value;
+      console.log(`selected ${value}`);
+    }
+    const handleChangeSchoolSection = (value: any) =>{
+      data.schoolsectionVal=value;
+      console.log(`selected ${value}`);
+    }
 
     const created = (): void => {
-      let iname = {"name" :"t33336553"};
+      let iname = {"name" :data.inputVal};
       axios.post(process.env.VUE_APP_SERVER + "/game/create",iname).then((res: any) => {
         let game = res.data;
         console.log("data",data);
         if (game.success) {
+          let stepList: Array<any> = data.stepList;
           data.editorUrl = game.data.editorUrl;
           console.log("editorUrl",data.editorUrl);
+          data.stepStatus = 1;
+          stepList[0].status = 'finish';
+          stepList[1].status = 'process';
         }
       })
     };
 
+    
     const published = (): void => {
-      let req = {"subject" :"1","schoolsection" :"1"};
+      let req = {"subject" :data.subjectVal,"schoolsection" :data.schoolsectionVal};
       axios.post(process.env.VUE_APP_SERVER + "/game/publish",req).then((res: any) => {
         let game1 = res.data;
         console.log("data",data);
         if (game1.success) {
           data.gameUrl = game1.data.gameUrl;
+          let stepList: Array<any> = data.stepList;
+          stepList[2].status = 'finish';
+          data.stepStatus = 3;
         }
       })
     };
 
     const nextStep1 = ():void =>{
-      let stepList: Array<any> = data.stepList;
+      data.stepStatus = 4;
       created();
-      stepList[0].status = 'finish';
-      stepList[1].status = 'process';
-      data.stepStatus = 1;
+      
     }
     
     const nextStep2 = ():void =>{
@@ -139,17 +162,19 @@ export default defineComponent({
     }
 
     const nextStep3 = ():void =>{
+      data.stepStatus = 4;
       published();
-      let stepList: Array<any> = data.stepList;
-      stepList[2].status = 'finish';
-      data.stepStatus = 3;
+      
     }
     
     return {
       ...toRefs(data),
       nextStep1,
       nextStep2,
-      nextStep3
+      nextStep3,
+      onChange,
+      handleChangeSubject,
+      handleChangeSchoolSection
     }
 
   },
